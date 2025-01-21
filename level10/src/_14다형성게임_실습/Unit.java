fb4e34d5d9282aae6d0a7f011744c6798d32e2f5
Ziyoung-sheep <sohardworker2023@gmail.com>
2025-01-21 18:03:43 +0900

    [feat] todaylast

--- level10/src/_14다형성게임_실습/Unit.java
+++ level10/src/_14다형성게임_실습/Unit.java
@@ -2,4 +2,18 @@ package _14다형성게임_실습;
 
 public abstract class Unit {
 	
+	private String name;
+	private int hp;
+	private final int MAX_HP;
+	private int power;
+	
+	public Unit(String name, int hp, int mAX_HP, int power) {
+		super();
+		this.name = name;
+		this.hp = hp;
+		MAX_HP = mAX_HP;
+		this.power = power;
+	}
+	
+	
 }
